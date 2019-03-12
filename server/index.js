28a5238e7802e018ef6f0158e9abc249dd7e613d
linh4 <linh44h@gmail.com>
2019-03-12 15:25:47 -0400

    basic setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy())
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log('server starting...')
+});
